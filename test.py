7390bd786e9080283630d82599c90ada36a76460
jasonkim.0815@gmail.com <jasonkim.0815@gmail.com>
2020-12-15 01:24:40 +0000

    Update for demo
    demo

--- test.py
+++ test.py
@@ -5,3 +5,12 @@ sys.path.append("/Workspace/Projects/jason.kim@databricks.com/test")
 # COMMAND ----------
 
 import foo
+
+# COMMAND ----------
+
+from pathlib import Path
+
+print(Path("/Workspace/Projects/jason.kim@databricks.com/test/data.csv").read_text())
+
+# COMMAND ----------
+
